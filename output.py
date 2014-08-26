86ce1841fbc62c734235de660f90cff5835ace4e
seth-- <xd.seth@gmail.com>
2014-08-25 23:17:11 -0300

    comments

--- output.py
+++ output.py
@@ -19,6 +19,7 @@ class Table():
         return format.format(num, 'TB')
 
     def _sort(self, field):
+        # Used as key argument for sorted()
         order = ['code', 'lines', 'size', 'time', None, 'url', 'post_data']
         if field['name'] in order:
             return order.index(field['name'])
@@ -26,6 +27,7 @@ class Table():
             return order.index(None)
 
     def _print_first_line(self, result):
+        # The first line has the columns names
         self._first_line = False
         basic_widths = {'code': 4,
                        'lines': 8,
@@ -79,10 +81,8 @@ class Table():
                 output.append(format.format(field['value']))
             print(' | '.join(output) + "\n", end='')
 
-
-
-            #Orden: code, lines, size, time, OTHERS, url, query_string, post_data
     def print_summary(self):
+        # The last line
         print("\nHidden: " + str(self._hidden_results) + "\n", end='')
         if self._aborted_jobs > 0:
             print("Aborted jobs: " + str(self._aborted_jobs) + "\n", end='')
